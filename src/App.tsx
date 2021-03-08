4e11b657fabf95e4fc5f67cfd268f8522b761400
Tiago <tiago.leonel@marttech.com.br>
2021-03-08 10:45:02 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import {
+
+	View,
+	Text,
+
+} from 'react-native';
+
+
+const App = () => {
+	return (
+		<View>
+			<Text>Hello World</Text>
+		</View>
+	);
+};
+
+export default App;
